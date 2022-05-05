ac359c1fd1f47f51d103633b871066565802230c
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-05 12:27:25 -0600

    export function 1

---
+++ pets.js
@@ -0,0 +1,12 @@
+export const pets = [
+    {
+        name: Arya,
+        age: 7,
+        breed: CoonHound,
+        image: './assets/Arya.jpeg',
+    },
+    { name:'Boulder', age: 3, Breed: 'Pit-mix' image: './assets/Boulder.jpeg' },
+    { name:'Crowley', age: 5, Breed: 'Red-Earred-Slider' image: './assets/Crowley.jpeg' },
+    { name:'Alfred', age: 5, Breed: 'Plecco' image: './assets/Alfred.jpeg' },
+
+]
