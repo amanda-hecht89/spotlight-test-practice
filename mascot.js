866cbaaab62e9a62a60d38c49b0b894689763638
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-06 13:02:02 -0600

    fruit.js

--- mascot.js
+++ mascot.js
@@ -5,7 +5,7 @@ export const football = [
         animal: 'Spartan',
         image: './assets/MSU.jpg',
     },
-    { state:'Denver', team: 'Broncos', animal: 'Bronco', image: './assets/Denver.jpg', },
-    { state:'University', team: 'Georiga', animal: 'Bulldog', image: './assets/UGA.png', },
+    { state:'Denver', team: 'Broncos', animal: 'Bronco', image: './assets/Denver.jpg' },
+    { state:'University', team: 'Georiga', animal: 'Bulldog', image: './assets/UGA.png' },
 
 ];
